14be2c9023d09b64e1c86b3af1a75ad7bfba3432
dove <codegeekdove@gmail.com>
2023-11-27 12:02:25 +0800

    done coding the logic for budgetleft coloring ui effect

--- src/containers/TheLayout.jsx
+++ src/containers/TheLayout.jsx
@@ -6,12 +6,32 @@ import ExpenseIndicator from '../components/ExpenseIndicator'
 import ExpensList from '../components/ExpenseList'
 
 function TheLayout() {
-  const [totalBudget, setTotalBudget] = useState(0)
+  const [totalBudget, setTotalBudget] = useState(null)
   const [expenses, setExpenses] = useState([])
 
   const totalExpenses = expenses.reduce((acc, val) => acc + val.price, 0)
 
   const budgetLeftAmount = totalBudget - totalExpenses
+
+  const percentage = totalExpenses > 0 ? (totalExpenses / totalBudget) * 100 : null
+
+  let stat
+  // if percentage get to90 to 100 percent your budget is out
+  if (percentage >= 90) {
+    stat = 'danger'
+    // if percentage get 50 to 90 percent close to burn
+  } else if (percentage >= 50) {
+    stat = 'warning'
+  } else {
+    // if your budget is in full
+    stat = 'success'
+  }
+
+  const budgetStatus = {
+    success: 'success',
+    warning: 'warning',
+    danger: 'danger',
+  }[stat]
 
   return (
     <>
@@ -23,7 +43,7 @@ function TheLayout() {
         </Col>
         <Col lg={6} className="ps-4">
           <ExpenseIndicator variant="primary" budget={`Budget: ${totalBudget > 0 ? `₱ ${totalBudget}` : '₱ 0'}`} />
-          <ExpenseIndicator variant="success" budgetLeft={`Budget Left: ₱ ${budgetLeftAmount === totalBudget ? '0' : budgetLeftAmount}`} />
+          <ExpenseIndicator variant={budgetStatus} budgetLeft={`Budget Left: ₱ ${budgetLeftAmount === totalBudget ? 0 : budgetLeftAmount}`} />
           <ExpensList expenses={expenses} />
         </Col>
       </Row>
